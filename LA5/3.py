a2c35ce989882684599f7133c6c5af4b43d4e4dd
Aditya4453 <adityamodani06@gmail.com>
2024-09-09 20:23:23 +0530

    Update 3.py

--- LA5/3.py
+++ LA5/3.py
@@ -1,27 +1,16 @@
 n=int(input("Enter any number :"))
-l=[]
-d=[]
-nd=[]
-sum_of_divisible=0
-sum_of_not_divisible=0
-x=int(input("Enter any number :"))
-
+d=0
+nd=0
+x=0
 while x!=-999:
-    l.append(x)
     x=int(input("Enter any number :"))
-for i in l:
-    if i%n==0:
-        d.append(i)
+
+    if x%n==0:
+        d=d+1
+        print("--{x}")
     else:
-        nd.append(i)
-        
-for i in d:
-    sum_of_divisible=sum_of_divisible+i
-    
-    
-for i in nd:
-    sum_of_not_divisible=sum_of_not_divisible+i
-    
-               
-print("Summ of all divisible number :",sum_of_divisible)
-print("Summ of all not divisible number :",sum_of_not_divisible)
+        nd=nd+1
+
+print(d)
+print(nd)          
+
