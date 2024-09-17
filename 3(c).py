a2b92221cf6970f28e478cd9a2c5bef69280ea94
Aditya4453 <adityamodani06@gmail.com>
2024-09-17 13:28:43 +0530

    Create 3(c).py

---
+++ 3(c).py
@@ -0,0 +1,9 @@
+n=int(input("Enter:"))
+for i in range(1,n+1):
+    for x in range(n-i,0,-1):
+        print(" ",end=" ")
+    for x in range ((2*i)-1):
+        print("*",end=" ")
+    for x in range(n-i,0,-1):
+        print(" ",end=" ")
+    print()
