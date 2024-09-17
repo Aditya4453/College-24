07d15c9d6ffa3c37dd28b292188df93b24fecd3d
Aditya4453 <adityamodani06@gmail.com>
2024-09-17 15:18:44 +0530

    Update 6.py

--- LA5/6.py
+++ LA5/6.py
@@ -6,8 +6,8 @@ print(y,end=" ")
 
 i=0
 while i<(n+1):
-    c=x+y
-    print(c,end=" ")
-    x=y
+    c=x+y #calculation
+    print(c,end=" ") #printing fibonnaci of every number
+    x=y  #updating the values for further calculations
     y=c
     i=i+1
