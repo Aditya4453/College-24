88f2078fc4e070d0f7f6a04909b0776a115d3d39
Aditya4453 <adityamodani06@gmail.com>
2024-09-20 22:23:15 +0530

    Delete LA3 directory

--- LA3/2.py
+++
@@ -1,3 +0,0 @@
-n=int(input("Enter the length of side:"))
-area=((3*(3)**(1/2))/2)*((n)**2)
-print(area)
--- LA3/6.py
+++
@@ -1,4 +0,0 @@
-x=int(input("enter first number :"))
-y=int(input("enter a second number :"))
-print("after swapping:")
-print("value of first:",y,"and second:",x)
