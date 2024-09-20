426d5f0d8f8ffbafbc3365534b5446effcdf6a68
Aditya4453 <adityamodani06@gmail.com>
2024-09-20 22:23:24 +0530

    Delete LA4 directory

--- LA4/10.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter any number :"))
-if n>0:
-    i=1
-    while i<=20:
-        print(n,"X",i,"=",n*i)
-        i=i+1
--- LA4/2.py
+++
@@ -1,5 +0,0 @@
-p=input("Enter Password:")
-if p=="s3cr3t!P@ssw0rd":
-    print("Welcome")
-else:
-    print("Wrong Password!")
