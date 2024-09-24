279859f7ad0118c9e617ef1df8bbb327cf4ed893
Aditya4453 <adityamodani06@gmail.com>
2024-09-24 15:29:10 +0530

    Update 1(a).py

--- 1(a).py
+++ 1(a).py
@@ -1,5 +1,5 @@
-import math
-n=360
-print("Deg. ","Sin    ","Cos",sep=" ")
-for i in range(0,n+1,10):
-    print(i ,round(math.sin(i),4),round(math.cos(i),4),sep="   ")
+n=int(input("Enter the number :"))
+sum=0
+for i in range(1,n+1):
+    sum+=(1/i)
+print(roubd(sum,9))
